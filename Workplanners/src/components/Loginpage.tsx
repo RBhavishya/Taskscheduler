a56d806972ed01a8750b1b019f67182e04f8211a
RBhavishya <bhavishya.r@orotron.com>
2025-08-21 14:20:58 +0530

    FIX : fix the slack login issue

--- Workplanners/src/components/Loginpage.tsx
+++ Workplanners/src/components/Loginpage.tsx
@@ -1,15 +1,30 @@
 import * as React from "react";
-import { useNavigate, useLocation, useSearch } from "@tanstack/react-router";
+import {
+  useNavigate,
+  useLocation,
+  useSearch,
+  useParams,
+} from "@tanstack/react-router";
 import loginimage from "../assets/loginimage.png";
 import slackicon from "../assets/slackicon.svg";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 const Loginpage = () => {
+  console.log(window.location.href, "ggg001");
+  const location = useLocation();
+  const url = new URLSearchParams(window.location.search);
+  const code1 = url.get("code");
+  console.log(code1, url, "gggggg");
+  useEffect(() => {
+    console.log(url, location, "uuuuuuuuuu");
+  }, [url, location]);
   const navigate = useNavigate();
-  const search = useSearch({ strict: false });
+  const search = useSearch({ strict: false }) as any;
   const code = search?.code;
+  console.log(code, "code001");
 
+  console.log(url, "paaa");
   const slackAuthMutation = useMutation({
     mutationFn: async () => {
       const res = await fetch(
@@ -45,9 +60,8 @@ const Loginpage = () => {
     onSuccess: (data) => {
       if (data?.status === "success") {
         localStorage.setItem("user", JSON.stringify(data.user));
+        console.log("success001");
         navigate({ to: "/dashboard" });
-      } else {
-        navigate({ to: "/" });
       }
     },
     onError: () => {
