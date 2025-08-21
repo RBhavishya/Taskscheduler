3346af06cf2d25dd90bad3ba6b38455cd38c508d
RBhavishya <bhavishya.r@orotron.com>
2025-08-21 17:32:02 +0530

    bha

--- Workplanners/src/components/Loginpage.tsx
+++ Workplanners/src/components/Loginpage.tsx
@@ -11,47 +11,36 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 const Loginpage = () => {
-  console.log(window.location.href, "ggg001");
   const location = useLocation();
   const url = new URLSearchParams(window.location.search);
   const code1 = url.get("code");
-  console.log(code1, url, "gggggg");
   const [code2, setCode2] = React.useState<string>();
   const navigate = useNavigate();
   const search = useSearch({ strict: false }) as any;
   const code = search?.code;
-  console.log(code, "code001");
   useEffect(() => {
-    console.log(url, location, "useeffectuuuuuuuuu");
     const code = search?.code;
     if (code) {
       setCode2(code);
     }
   }, [url, location, search]);
-  console.log(url, "paaa");
   const slackAuthMutation = useMutation({
     mutationFn: async () => {
       const res = await fetch(
         "https://api-task-sheduler-org.onrender.com/v1.0/auth/slack",
         { method: "GET" }
       );
-      console.log("frst api calleddddddddddddd", res);
-      // if (!res.ok) throw new Error("Unable to get Slack Auth URL");
       return res.json();
     },
     onSuccess: (data) => {
-      console.log(data?.data?.authUrl, "apidata");
       if (data?.data?.authUrl) {
-        console.log("frst api on successuuuuu", data?.data?.authUrl);
         window.location.href = data?.data?.authUrl;
       } else {
-        console.log("frst api on errorrrrrrrrr");
         alert("Unable to start Slack login. Please try again.");
       }
     },
     onError: () => {
       alert("Slack login error. Please try again.");
-      console.log("on errorr iruku");
     },
   });
   const slackCallbackMutation = useMutation({
@@ -62,34 +51,23 @@ const Loginpage = () => {
           method: "GET",
         }
       );
-      console.log(res, "apidata0021");
-      console.log("2nd api calleduuuuuuu");
-      // if (!res.ok) throw new Error("slack callback failed");
-      console.log("2nd api returned responseeuuuuuuuuuuu");
-      console.log(res, "ressss002");
+
       return res.json();
     },
     onSuccess: (data) => {
-      console.log(data, "apidata0022");
       if (data?.status === 200) {
-        console.log("2nd api succesuuuuuu");
         localStorage.setItem("user", JSON.stringify(data?.user));
-        console.log(
-          "user data stored in loal storageuuuu and need to navigate to dashboard"
-        );
+
         navigate({ to: "/dashboard" });
       }
     },
     onError: (err) => {
-      console.log("2nd api on errorrr haii", err);
       navigate({ to: "/" });
     },
   });
   useEffect(() => {
     if (code2) {
-      console.log(code2, "code222 vachindeeeee");
       slackCallbackMutation.mutate(code2);
-      console.log("hogaya!!!!!!!!");
     }
   }, [code2]);
   return (
